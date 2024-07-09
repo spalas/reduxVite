b4f53074c2963adb34069b107949e5d14388a1c6
palash sarkar <spalash521@gmail.com>
2024-07-09 22:29:29 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Addtodo from './compoments/Addtodo'
+import Todos from './compoments/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>redux toolkit</h1>
+      <Addtodo></Addtodo>
+      <Todos></Todos>
+    </>
+  )
+}
+
+export default App
